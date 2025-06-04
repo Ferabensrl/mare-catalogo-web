60de1ed06080166093470f144b831d86afea7f89
Ferabensrl <fnosieski@gmail.com>
2025-06-04 19:27:40 -0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    domains: ['ferabensrl.github.io', 'via.placeholder.com']
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
